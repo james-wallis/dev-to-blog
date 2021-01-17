ca3d418e05ade4b4e5bf65fed7366e1a35af091f
James Wallis <j@wallis.dev>
2021-01-17 00:09:33 +0000

    Add blog loaded from dev.to
    Signed-off-by: James Wallis <j@wallis.dev>

---
+++ pages/[slug].js
@@ -0,0 +1,78 @@
+import fs from 'fs';
+import path from 'path';
+import { getAllArticlesAndMinifyForCache, getArticleFromCache } from '../lib/devto';
+
+import Nav from '../components/nav'
+
+
+// This file is used for caching the article ID and local slug
+const cacheFile = '.dev-to-cache.json'; // Add this file to the .gitignore
+
+const ArticlePage = ({ article }) => (
+    <div>
+        <Nav />
+        <main>
+            <div className="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative h-96">
+                <div className="absolute left-0 bottom-0 w-full h-full z-10" style={{ backgroundImage: 'linear-gradient(180deg,transparent,rgba(0,0,0,.7))' }} />
+                {article.cover_image && <img src={article.cover_image} className="absolute left-0 top-0 w-full h-full z-0 object-cover" />}
+                <div className="p-4 absolute bottom-0 left-0 z-20">
+                    <span className="px-4 py-1 bg-black text-gray-200 items-center justify-center mb-2">
+                        {article.tag_list}
+                    </span>
+                <h2 className="text-4xl font-semibold text-gray-100 leading-tight">
+                    {article.title}
+                </h2>
+                <div className="flex mt-3">
+                    <p className="font-semibold text-gray-200 text-sm">
+                        {article.readable_publish_date}
+                    </p>
+                </div>
+                </div>
+            </div>
+
+            <div className="px-4 lg:px-0 mt-8 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">
+                <div className="flex justify-center text-md my-4 italic text-gray-700 font-bold hover:underline">
+                    <a href={article.url} target="_blank">Also posted on Dev.to</a>
+                </div>
+                <div className="article" dangerouslySetInnerHTML={{ __html: article.body_html }} />
+                <div className="flex justify-center text-md mt-10 mb-32 italic text-gray-700 font-bold hover:underline">
+                    <a href={article.url} target="_blank">Like this article? React or comment on Dev.to</a>
+                </div>
+            </div>
+        </main>
+    </div>
+)
+
+// This function gets called at build time
+export async function getStaticPaths() {
+    // Get minified articles (just article ID and local slug) and cache them for use in getStaticProps
+    const minifiedArticles = await getAllArticlesAndMinifyForCache();
+
+    // Save minified article data to cache file
+    fs.writeFileSync(path.join(process.cwd(), cacheFile), JSON.stringify(minifiedArticles));
+
+    // Get the paths we want to pre-render based on posts
+    const paths = minifiedArticles.map(({ slug }) => {
+        return {
+            params: { slug },
+        }
+    })
+
+    // We'll pre-render only these paths at build time.
+    // { fallback: false } means other routes should 404.
+    return { paths, fallback: false }
+}
+
+// This also gets called at build time
+export async function getStaticProps({ params }) {
+    // Read cache and parse to object
+    const cacheContents = fs.readFileSync(path.join(process.cwd(), cacheFile));
+    const cache = JSON.parse(cacheContents);
+
+    // Using the cache, fetch the article from Dev.to
+    const article = await getArticleFromCache(cache, params.slug);
+
+    return { props: { article } }
+}
+
+export default ArticlePage;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,50 @@
-import Nav from '../components/nav'
+import Link from 'next/link';
+import Nav from '../components/nav';
+import { getAllArticles, convertCanonicalURLToRelative } from '../lib/devto';
 
-export default function IndexPage() {
+export default function IndexPage({ articles }) {
   return (
     <div>
       <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
+      <div className="py-20 flex flex-col justify-center items-center">
+        <h1 className="text-3xl">Welcome to my blog 👋</h1>
+        <p className="max-w-2xl text-center my-4">All the articles you see are dynamically loaded from Dev.to at build time. The website is refreshed whenever an article is created or updated.</p>
+        {articles.map(article => (
+          <Link href={convertCanonicalURLToRelative(article.canonical_url)} key={article.id}>
+            <a className="mt-6 w-full flex flex-col items-center">
+                <div className="lg:w-2/5 md:w-3/5 w-4/5 px-10 py-6 bg-white rounded-lg shadow-md">
+                    <div className="flex justify-between items-center">
+                        <span className="font-light text-gray-600">
+                            {article.readable_publish_date}
+                        </span>
+                        <span className="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">
+                            {article.tags}
+                        </span>
+                    </div>
+                    <div className="mt-2">
+                        <h3 className="text-2xl text-gray-700 font-bold hover:underline">
+                            {article.title}
+                        </h3>
+                        <p className="mt-2 text-gray-600">
+                            {article.description}
+                        </p>
+                    </div>
+                    <div className="flex justify-between items-center mt-4">
+                        Read more
+                    </div>
+                </div>
+            </a>
+          </Link>
+        ))}
       </div>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  // Get all the articles that have a canonical URL pointed to your blog
+  const articles = await getAllArticles();
+
+  // Pass articles to the page via props
+  return { props: { articles } };
 }
